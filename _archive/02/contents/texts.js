2a2229733ab09c57f5ca1e30d9c2881e45dd67d4
guillaume3 <guillaume.3.7.13@gmail.com>
2018-12-14 12:52:04 +0100

    first attemps with javascript text aborted

---
+++ _archive/02/contents/texts.js
@@ -0,0 +1,31 @@
+
+
+var texts = {
+
+  mainTitle: {
+    EN: "HawAI.tech",
+    FR: "HawAI.tech"
+  },
+  mainSubtitle: {
+    tag: "h3",
+    EN: "Hardware for AI",
+    FR: "Hardware pour l'AI"
+  },
+  visionTitle: {
+    EN: "Machines for Probabilist IA",
+    FR: "Machines pour l'IA Probabiliste"
+  },
+  item_rebootTitle: {
+    EN: "Rebooting computing",
+    FR: "Rebooting computing"
+  },
+  item_rebootText: {
+    EN: "The glorious era of von Neuman architectured computers is behind us.",
+    FR: "L’ère glorieuse des ordinateurs d’architecture von Neumann est derrière nous. Ce type de machine a atteint ses limites, tant du point de vue technologique que de celui des applications. Une grande effervescence (rebooting computing) règne dans le monde du traitement de l’information pour essayer d’imaginer et de développer des concepts alternatifs."
+  },
+  serviceTitle: {
+    EN: "Boost MCMC Algorithms",
+    FR: "Boostez les Algorithmes MCMC"
+  }
+
+};
